ff721af00159fc8c1706b117def1db8638e20c52
matheodino <matheomatheo07@gmail.com>
2022-09-22 16:58:28 +0200

    First styles

--- inc/header.php
+++ inc/header.php
@@ -1,11 +1,13 @@
-<img src="../images/logo-bella-yoga.png" alt="Logo Bella Yoga">
-
-<nav>
-    <ul>
-        <li>Accueil</li>
-        <li>À propos</li>
-        <li>Cours</li>
-        <li>Contact</li>
-        <li>Blog</li>
-    </ul>
-</nav>
+<header>
+    <img src="../images/logo-bella-yoga.png" alt="Logo Bella Yoga">
+    
+    <nav>
+        <ul>
+            <li>Accueil</li>
+            <li>À propos</li>
+            <li>Cours</li>
+            <li>Contact</li>
+            <li>Blog</li>
+        </ul>
+    </nav>
+</header>
