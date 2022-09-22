3fd3878b189b4b8f706b451969c9745666cea8f1
matheodino <matheomatheo07@gmail.com>
2022-09-22 14:51:01 +0200

    Skeleton import

---
+++ inc/head.php
@@ -0,0 +1,10 @@
+<!-- Settings -->
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Imports -->
+<link rel="stylesheet" href="/style/style.css">
+<!-- Favicon -->
+<link rel="icon" href="/images/favicon.png" type="image/png">
+<!-- Author -->
+<meta name="author" content="matheodino, hi@matheonlht.com">
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include 'inc/head.php' ?>
+    <!-- Page title -->
+    <title>Home – Site</title>
+</head>
+
+<body>
+    <!-- --- HEADER --- -->
+    <?php include 'inc/header.php'; ?>
+
+    <!-- --- MAIN --- -->
+    <main></main>
+
+    <!-- --- FOOTER --- -->
+    <?php include 'inc/footer.php'; ?>
+</body>
+
+</html>
