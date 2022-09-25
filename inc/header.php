78229c1285d98338f1dc2a6347785d8c6927cf61
matheodino <matheomatheo07@gmail.com>
2022-09-25 17:58:29 +0200

    Dynamic menu

--- inc/header.php
+++ inc/header.php
@@ -3,11 +3,11 @@
 
     <nav>
         <ul>
-            <li>Accueil</li>
-            <li>À propos</li>
-            <li>Cours</li>
-            <li>Contact</li>
-            <li>Blog</li>
+            <li><a href="/" <?php if ($slug == 'index') echo $active; ?>>Accueil</a></li>
+            <li><a href="/a-propos.php" <?php if ($slug == 'a-propos') echo $active; ?>>À propos</a></li>
+            <li><a href="/cours.php" <?php if ($slug == 'cours') echo $active; ?>>Cours</a></li>
+            <li><a href="/contact.php" <?php if ($slug == 'contact') echo $active; ?>>Contact</a></li>
+            <li><a href="/blog.php" <?php if ($slug == 'blog') echo $active; ?>>Blog</a></li>
         </ul>
     </nav>
 </header>
