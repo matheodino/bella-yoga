af470067eb0ac67a2f21e5edeccebeb30cb4e003
matheodino <matheomatheo07@gmail.com>
2022-09-30 15:44:58 +0200

    Pagination minor

--- blog/index.php
+++ blog/index.php
@@ -41,8 +41,9 @@
 
             <!-- Pagination -->
             <div class="pagination flex">
-                <button class="page active">1</button>
-                <!-- <button class="page">2</button> -->
+                <button class="active">1</button>
+                <button>2</button>
+                <button>3</button>
             </div>
         </section>
     </main>
